4a175b26ed0e3a9839e4f5c3f2614f4f193f06cb
anyamiyeth <anyamiyeth@byasystems.com.co>
2020-10-26 11:34:33 -0500

    Agregar archivos de proyecto.

---
+++ PresentacionGUI/FrmGestionPersona.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PresentacionGUI
+{
+    partial class FrmGestionPersona
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGestionPersona));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtIdentificacion = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPulsacion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CmbSexo = new System.Windows.Forms.ComboBox();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Identificacion";
+            // 
+            // TxtIdentificacion
+            // 
+            this.TxtIdentificacion.Location = new System.Drawing.Point(117, 128);
+            this.TxtIdentificacion.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtIdentificacion.Name = "TxtIdentificacion";
+            this.TxtIdentificacion.Size = new System.Drawing.Size(133, 20);
+            this.TxtIdentificacion.TabIndex = 1;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(117, 160);
+            this.TxtNombre.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(133, 20);
+            this.TxtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.Location = new System.Drawing.Point(117, 190);
+            this.TxtEdad.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(133, 20);
+            this.TxtEdad.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 192);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 222);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "sexo";
+            // 
+            // TxtPulsacion
+            // 
+            this.TxtPulsacion.Enabled = false;
+            this.TxtPulsacion.Location = new System.Drawing.Point(117, 251);
+            this.TxtPulsacion.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.TxtPulsacion.Name = "TxtPulsacion";
+            this.TxtPulsacion.Size = new System.Drawing.Size(133, 20);
+            this.TxtPulsacion.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 253);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Pulsacion";
+            // 
+            // CmbSexo
+            // 
+            this.CmbSexo.FormattingEnabled = true;
+            this.CmbSexo.Location = new System.Drawing.Point(117, 220);
+            this.CmbSexo.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.CmbSexo.Name = "CmbSexo";
+            this.CmbSexo.Size = new System.Drawing.Size(133, 21);
+            this.CmbSexo.TabIndex = 10;
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.AutoSize = true;
+            this.BtnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("BtnGuardar.Image")));
+            this.BtnGuardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnGuardar.Location = new System.Drawing.Point(36, 302);
+            this.BtnGuardar.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(134, 134);
+            this.BtnGuardar.TabIndex = 11;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.BtnGuardar.UseVisualStyleBackColor = true;
+            this.BtnGuardar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(511, 93);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(198, 302);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 134);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Guardar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmGestionPersona
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 462);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.CmbSexo);
+            this.Controls.Add(this.TxtPulsacion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtIdentificacion);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "FrmGestionPersona";
+            this.Text = "FrmGestionPersona";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtIdentificacion;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPulsacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CmbSexo;
+        private System.Windows.Forms.Button BtnGuardar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PresentacionGUI/FrmGestionPersona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entity;
+using BLL;
+
+namespace PresentacionGUI
+{
+    public partial class FrmGestionPersona : Form
+    {
+        public FrmGestionPersona()
+        {
+            InitializeComponent();
+            TxtPulsacion.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Identificacion = TxtIdentificacion.Text;
+            persona.Nombre = TxtNombre.Text;
+            persona.Edad = Convert.ToInt32(TxtEdad.Text);
+            persona.Sexo = CmbSexo.Text;
+            persona.CalcularPulsacion();
+            TxtPulsacion.Text = persona.Pulsacion.ToString();
+            PersonaService service = new PersonaService();
+            string mensaje=service.Guardar(persona);
+            MessageBox.Show(mensaje);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
